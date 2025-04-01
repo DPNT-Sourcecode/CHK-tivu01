c95f4cadd47db289db582161af6ff971fac2e6a3
sbx_user1051 <sbx_user1051@169.254.47.151>
2025-04-01 11:17:32 +0000

    Tue Apr 01 11:17:32 UTC 2025

--- test/solution_tests/CHK/test_sum.py
+++ test/solution_tests/CHK/test_sum.py
@@ -4,3 +4,15 @@ from solutions.CHK import checkout_solution
 class TestCheckout():
     def test_checkout_no_offer(self):
         assert checkout_solution.checkout('AA') == 100
+
+    def test_checkout_with_offer(self):
+        assert checkout_solution.checkout('AAA') == 130
+
+    def test_checkout_multi_offer(self):
+        assert checkout_solution.checkout('AAABB') == (130 + 45)
+
+    def test_checkout_half_offer(self):
+        assert checkout_solution.checkout('AAAA') == (130 + 50)
+
+    def test_checkout_invalid(self):
+        assert checkout_solution.checkout('AX') == 
