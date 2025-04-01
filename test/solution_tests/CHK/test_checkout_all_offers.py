b52b6fc863c94e0cbc20da85f1c40f2a0afa3824
sbx_user1051 <sbx_user1051@169.254.30.159>
2025-04-01 12:46:26 +0000

    Tue Apr 01 12:46:26 UTC 2025

--- test/solution_tests/CHK/test_checkout_all_offers.py
+++ test/solution_tests/CHK/test_checkout_all_offers.py
@@ -6,4 +6,22 @@ class TestCheckoutAllOffers():
         for item in prices:
             assert checkout_solution.checkout(item) == prices[item]
     
-    def 
+    def test_checkout_multi_offers(self):
+        for item, offers in multi_offers.items():
+
+            # Apply single multi-priced offer
+            for offer_qty, offer_price in offers:
+                assert checkout_solution.checkout(item * offer_qty) == offer_price 
+                assert checkout_solution.checkout(item * (offer_qty + 1)) == (offer_price + prices[item])
+
+            skus = ''
+            total_offer_price = 0
+            # Apply multiple multi-priced offer
+            for offer_qty, offer_price in offers:
+                skus = item * offer_qty
+                total_offer_price += offer_price
+                
+            assert checkout_solution.checkout(skus) == total_offer_price 
+            assert checkout_solution.checkout(skus + item) == (total_offer_price + prices[item])
+
+    # def test_checkout_free_item_offers(self):
