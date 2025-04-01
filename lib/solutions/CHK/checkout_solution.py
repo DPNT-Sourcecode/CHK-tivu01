c14b0217ce64bd1ac7f05a762fd069d563165ec6
sbx_user1051 <sbx_user1051@169.254.55.17>
2025-04-01 12:33:41 +0000

    Tue Apr 01 12:33:41 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,15 +4,50 @@
 # skus = unicode string
 def checkout(skus):
 
-    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10}
+    prices = {
+        'A': 50, 
+        'B': 30, 
+        'C': 20, 
+        'D': 15, 
+        'E': 40, 
+        'F': 10,
+        'G': 20,
+        'H': 10,
+        'I': 35,
+        'J': 60,
+        'K': 80,
+        'L': 90,
+        'M': 15,
+        'N': 40,
+        'O': 10,
+        'P': 50,
+        'Q': 30,
+        'R': 50,
+        'S': 30,
+        'T': 20,
+        'U': 40,
+        'V': 50,
+        'W': 20,
+        'X': 90,
+        'Y': 10,
+        'Z': 50,}
     offers = {
         'A': [(5, 200), (3, 130)], # 3A for 130
         'B': [(2, 45)],  # 2B for 45   
+        'H': [(10, 80), (5, 45)], # 5H for 45
+        'K': [(2, 150)], # 2K for 150
+        'P': [(5, 200)], # 5p for 200
+        'Q': [(3, 80)], # 3Q for 80
+        'V': [(2, 90), (3, 130)], # 5H for 45
     }
 
     free_item_offers = {
         'E': (2, 1, 'B'), # 2E for 1 free B
         'F': (2, 1, 'F'), # 2F for 1 free F
+        'N': (3, 1, 'M'), # 3M for 1 free N
+        'R': (3, 1, 'Q'), # 3R for 1 free Q
+        'U': (3, 1, 'U'), # 3U for 1 free U
+
     }
     
     # !!! Caution when changing the prices !!!
@@ -65,6 +100,7 @@ def checkout(skus):
     return total
     
     
+
 
 
 
